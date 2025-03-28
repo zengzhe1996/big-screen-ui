c370bb23528bfc424c41d9686a6a6659252efe15
曾哲 <29989719@qq.com>
2025-03-28 16:26:35 +0800

    feat: unocss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 
 defineProps({
-  msg: String,
+  msg: String
 })
 
 const count = ref(0)
@@ -10,7 +10,6 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
