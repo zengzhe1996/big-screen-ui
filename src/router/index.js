e6138f0ffb7bf5c33ec9dec2027d049b8cfde47b
曾哲 <29989719@qq.com>
2025-03-31 11:05:51 +0800

    fix: 路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: () => import('@/views/index.vue') },
@@ -7,6 +7,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
